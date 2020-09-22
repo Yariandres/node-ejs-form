d481fbf9acb0ea93fe553cfd8e5e948fbc192e38
Yari Andres <yari.andres@gmail.com>
2020-09-22 23:49:41 +0200

    adding final form and app

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.set('view-engine', 'ejs');
-app.set('view', 'views');
+const bodyParser = require('body-parser');
 
+const users = [];
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
-  res.render();
+  res.render('index', { pageTitle: 'Add User' });
 });
-app.get('/users', (req, res) => {
-  res.render();
+
+app.get('/users', (req, res) => {  
+  res.render('users', { pageTitle: 'User', users: users });
 });
+
 app.post('/add-user', (req, res) => {
-  res.redirect('/users')
+  users.push({name: req.body.username});
+  res.redirect('/users');
 });
 
 
